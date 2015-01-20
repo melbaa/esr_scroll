ed7474df430f245e22408ed4810d73eac73d38fc
melbaa <none@no-reply>
2015-01-20 21:33:07 +0200

    pedantry

--- mel.user.js
+++ mel.user.js
@@ -15,7 +15,7 @@ var UP = 38;
 
 function scrollTo(selector, elem_num) {
     var comments = $(selector);
-    
+    if (comments.length == 0) return -1;
     
     elem_num = Math.max(0, elem_num)
     elem_num = Math.min(comments.length-1, elem_num);
@@ -23,6 +23,7 @@ function scrollTo(selector, elem_num) {
     console.log(elem_num)
     return elem_num
 }
+
 
 $(window).load(function() {
     $("html").keyup(function(evt) {
